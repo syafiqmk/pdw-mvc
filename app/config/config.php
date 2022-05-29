8287059247d6f14abce2ec1a134471ef183d3cf8
syafiqmk <syafiqmuh.k@gmail.com>
2022-05-29 19:19:56 +0800

    setting Config

---
+++ app/config/config.php
@@ -0,0 +1,11 @@
+<?php 
+
+    //base url
+    define("BASE_URL", "http://localhost/mvc-template/public");
+
+    //const database
+    define("DB_HOST", "");
+    define("DB_USER", "");
+    define("DB_PASS", "");
+    define("DB_NAME", "");
+?>
