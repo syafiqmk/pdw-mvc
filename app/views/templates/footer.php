84159af74797ea25888c06163ca478ec83cf543c
syafiqmk <syafiqmuh.k@gmail.com>
2022-05-29 19:50:54 +0800

    setting header & footer

---
+++ app/views/templates/footer.php
@@ -0,0 +1,6 @@
+    <!-- koneksi javascript -->
+    <script src="js/jquery-3.6.0.js"></script>
+    <script src="js/bootstrap.bundle.js"></script>
+    <script src="js/script.js"></script>
+</body>
+</html>
