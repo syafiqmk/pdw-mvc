cff988d343634a538c6af951bf74af2f872a16fa
syafiqmk <syafiqmuh.k@gmail.com>
2022-05-29 19:43:42 +0800

    setting init

---
+++ app/init.php
@@ -0,0 +1,11 @@
+<?php 
+
+    //core
+    require_once "core/App.php";
+    require_once "core/Controller.php";
+    require_once "core/Database.php";
+    require_once "core/Flasher.php";
+
+    //config
+    require_once "config/config.php";
+?>
