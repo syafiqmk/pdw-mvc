9893bc90feb1e7847c2b09e3e5b94311b8f923b2
syafiqmk <syafiqmuh.k@gmail.com>
2022-05-29 19:46:29 +0800

    setting public/index

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<?php 
+
+    //turn error reporting off
+    error_reporting(0);
+
+    //check session
+    if(!session_id()) {
+        //session start
+        session_start();
+    }
+
+    //require init
+    require_once "../app/init.php";
+    $app = new App;
+?>
