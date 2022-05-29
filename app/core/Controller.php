135d7a69d4ccd3a8b710f1e59b9e4d8e14abea4e
syafiqmk <syafiqmuh.k@gmail.com>
2022-05-29 18:54:57 +0800

    setting class Controller

---
+++ app/core/Controller.php
@@ -0,0 +1,15 @@
+<?php 
+
+    class Controller {
+
+        //method view
+        public function view($view, $data = []) {
+            require_once "../app/views/".$view.".php";
+        }
+
+        //method model
+        public function model($model) {
+            require_once "../app/models/".$model.".php";
+        }
+    }
+?>
