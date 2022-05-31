aec54116387a8e86dbf40f1adf5c4a69588df92d
syafiqmk <syafiqmuh.k@gmail.com>
2022-05-31 18:42:32 +0800

    update config

--- app/config/config.php
+++ app/config/config.php
@@ -1,7 +1,7 @@
 <?php 
 
     //base url
-    define("BASE_URL", "http://localhost/mvc-template/public");
+    define("BASE_URL", "http://localhost/ti2022/db-mvc/public");
 
     //const database
     define("DB_HOST", "localhost");
